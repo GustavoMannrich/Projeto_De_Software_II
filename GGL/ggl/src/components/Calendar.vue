a7e0cc50b21a8501c33ede077c6f5158047fb321
ReinerTz <llreinert@gmail.com>
2019-04-25 19:43:12 -0300

    dashboard e tela de login

---
+++ GGL/ggl/src/components/Calendar.vue
@@ -0,0 +1,95 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-layout>
+      <v-flex>
+        <v-sheet height="500">
+          <v-calendar
+            :now="today"
+            :value="today"
+            color="primary"
+          >
+            <template v-slot:day="{ date }">
+              <template v-for="event in eventsMap[date]">
+                <v-menu
+                  :key="event.title"
+                  v-model="event.open"
+                  full-width
+                  offset-x
+                >
+                  <template v-slot:activator="{ on }">
+                    <div
+                      v-if="!event.time"
+                      v-ripple
+                      class="my-event"
+                      v-on="on"
+                      v-html="event.title"
+                    ></div>
+                  </template>
+                  <v-card
+                    color="grey lighten-4"
+                    min-width="350px"
+                    flat
+                  >
+                    <v-toolbar
+                      color="primary"
+                      dark
+                    >
+                      <v-btn icon>
+                        <v-icon>edit</v-icon>
+                      </v-btn>
+                      <v-toolbar-title v-html="event.title"></v-toolbar-title>
+                      <v-spacer></v-spacer>
+                      <v-btn icon>
+                        <v-icon>favorite</v-icon>
+                      </v-btn>
+                      <v-btn icon>
+                        <v-icon>more_vert</v-icon>
+                      </v-btn>
+                    </v-toolbar>
+                    <v-card-title primary-title>
+                      <span v-html="event.details"></span>
+                    </v-card-title>
+                    <v-card-actions>
+                      <v-btn
+                        flat
+                        color="secondary"
+                      >
+                        Cancel
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-menu>
+              </template>
+            </template>
+          </v-calendar>
+        </v-sheet>
+      </v-flex>
+    </v-layout>
+  </v-app>
+</div>
+</template>
+
+<style>
+    .my-event {
+    overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #ffffff;
+  border: 1px solid #1867c0;
+  width: 100%;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+}
+</style>
+
+<script>
+export default {
+  name: 'Calendar'
+}
+
+</script>
